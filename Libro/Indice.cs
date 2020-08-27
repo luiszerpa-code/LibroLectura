70798836b3dd6a7c566fa4a48218070c3a27e583
luiszerpa-code <63805299+luiszerpa-code@users.noreply.github.com>
2020-08-27 11:59:50 -0500

    Add files via upload

---
+++ Libro/Indice.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Libro
+{
+    public class Indice
+    {
+        public List<Letra> Lecciones { get; set; }
+
+        public string Leer { get; set; }
+
+        public Indice()
+        {
+            this.Lecciones = new List<Letra>();
+
+            Letra letraA = new Letra();
+            letraA.Codigo = "A";
+            letraA.Lectura = "A - A - A    Avion";
+            letraA.Cantidad = 1;
+
+            Letra letraE = new Letra();
+            letraE.Codigo = "E";
+            letraE.Lectura = "E - E - E    Elefante";
+            letraE.Cantidad = 1;
+
+            Letra letraI = new Letra();
+            letraI.Codigo = "I";
+            letraI.Lectura = "I - I - I    Iglesia";
+            letraI.Cantidad = 1;
+
+            Letra letraO = new Letra();
+            letraO.Codigo = "O";
+            letraO.Lectura = "O - O - O    Oso";
+            letraO.Cantidad = 1;
+
+            Letra letraU = new Letra();
+            letraU.Codigo = "U";
+            letraU.Lectura = "U - U - U    Uva";
+            letraU.Cantidad = 1;
+
+            this.Lecciones.Add(letraA);
+            this.Lecciones.Add(letraE);
+            this.Lecciones.Add(letraI);
+            this.Lecciones.Add(letraO);
+            this.Lecciones.Add(letraU);
+        }
+
+        public int validaLetra(string codigo)
+        {
+            int encontro = -1;
+
+            for (int i = 0; i<this.Lecciones.Count; i++)
+            {
+                if (this.Lecciones[i].Codigo == codigo)
+                {
+                    encontro = i;
+                }
+            }
+
+            return encontro;
+        }
+
+        public bool agregarLetra(Letra letra)
+        {
+            int enc = this.validaLetra(letra.Codigo);
+            if (enc >= 0)
+            {
+                this.Lecciones[enc].sumarCantidad(letra.Cantidad);
+            }
+            else
+            {
+                this.Lecciones.Add(letra);
+            }
+            return true;
+        }
+
+        public bool eliminarLetra(string codigo)
+        {
+            int enc = this.validaLetra(codigo);
+            
+            if(enc >= 0)
+            {
+                this.Lecciones.RemoveAt(enc);
+                return true;
+            }
+
+            return false;
+        }
+
+
+        public Letra leyendo(string codigo)
+        {
+            int enc = this.validaLetra(codigo);
+            if (enc >= 0)
+            {
+                if (this.Lecciones[enc].validarCantidad())
+                {
+                    return this.Lecciones[enc];
+                }
+            }
+            return null;
+        }
+
+        public string listadoLecciones()
+        {
+            string lista = "";
+            foreach(Letra item in this.Lecciones)
+            {
+                lista += item.Codigo + " " + "\n";
+            }
+            return lista;
+        }
+
+    }
+}
---
+++ Libro/Letra.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Libro
+{
+    public class Letra
+    {
+        public string Codigo { get; set; }
+        public string Lectura { get; set; }
+        public int Cantidad { get; set; }
+
+        public void sumarCantidad(int cantidad)
+        {
+            this.Cantidad += cantidad;
+        }
+
+        public bool validarCantidad()
+        {
+            if(this.Cantidad > 0)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
